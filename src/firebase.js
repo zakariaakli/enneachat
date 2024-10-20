5a39f138011fd804238aec21a16ef799f9d0feb2
Zakaria <zakaria.akli.ensa@gmail.com>
2024-10-20 18:04:25 +0200

    Update config for firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDPxy4xlCYUN0XfXtQsx8xYWO1AeCdqKGE",
-  authDomain: "enneachat-3883f.firebaseapp.com",
-  projectId: "enneachat-3883f",
-  storageBucket: "enneachat-3883f.appspot.com",
-  messagingSenderId: "157195361830",
-  appId: "1:157195361830:web:23d4f488cbfedcd63390e7",
-  measurementId: "G-MYJ9C0ESNJ"
+  apiKey: process.env.FIREBASE_apiKey,
+  authDomain: process.env.FIREBASE_authDomain,
+  projectId: process.env.FIREBASE_projectId,
+  storageBucket: process.env.FIREBASE_storageBucket,
+  messagingSenderId: process.env.FIREBASE_messagingSenderId,
+  appId: process.env.FIREBASE_appId ,
+  measurementId: process.env.FIREBASE_measurementId
 };
 
 // Initialize Firebase
